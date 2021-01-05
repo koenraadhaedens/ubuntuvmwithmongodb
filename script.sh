716d1c7db8a5e39b58fffc2d2d391ab31e216da6
koenraadhaedens <40601026+koenraadhaedens@users.noreply.github.com>
2021-01-05 11:07:48 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-script to install all on the ubuntu
+#!/bin/sh
+echo "Install Mongodb ..."
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -apt-key list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl status mongod
+sudo systemctl enable mongod
